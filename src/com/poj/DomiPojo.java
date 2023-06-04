4c9353bca6d7b894c1b019858fab963fc7c6fb3f
jeeban733 <jeebanp733@gmail.com>
2023-06-04 06:41:21 +0530

    new added

--- src/com/poj/DomiPojo.java
+++ src/com/poj/DomiPojo.java
@@ -2,7 +2,8 @@ package com.poj;
 
 public class DomiPojo {
 	public static void main(String[] args) {
-	System.out.print("hi");
+	System.out.print("hi milan");
+    System.out.println("hi");
 	}
 
 }
