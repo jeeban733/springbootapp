1e51ae17b2d317aacc76b18e449b3cc0197998f2
jeeban733 <jeebanp733@gmail.com>
2023-06-04 06:29:13 +0530

    wh-9098 new commit and push

--- src/com/poj/DomiPojo.java
+++ src/com/poj/DomiPojo.java
@@ -2,7 +2,7 @@ package com.poj;
 
 public class DomiPojo {
 	public static void main(String[] args) {
-		
+	System.out.print("hi");
 	}
 
 }
---
+++ src/com/poj/model/ModelEmp.java
@@ -0,0 +1,5 @@
+package com.poj.model;
+
+public class ModelEmp {
+
+}
